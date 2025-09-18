d61e9a60a614d7594bb9bbf9bd98b55b5ecc297a
Chris Haikney <chaikney@c4r6s3.42urduliz.com>
2025-09-18 14:46:51 +0200

    Errors: use std::invalid_argument for parameters
    Reduces duplication of error/return blocks.
    Could further improve readability by making a check_parameters function

--- main.cpp
+++ main.cpp
@@ -12,44 +12,38 @@
 // Create a Server instance with those arguments
 // TODO Add signal handlers to cleanly exit on SIGINT and SIGTERM
 // TODO Add error handling for invalid arguments
-// TODO Add error handling for Server constructor problems
+// DONE Add error handling for Server constructor problems
 int	main(int argc, char **argv)
 {
 	std::string password;
 	int port_num;
-	if (argc < 3)
-	{
-		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
-		return (1);
-	}
-	//haciendo isstringstream comprobamos automaticamente posibles errores de conversion y !portav >> port_num lo convierte a int
-	std::istringstream port_av(argv[1]);
-    if (!(port_av >> port_num) || port_num <= 0 || port_num > 65535)
-    {
-        std::cerr << "Invalid port number" << std::endl;
-        return (1);
-    }
-	password = argv[2];
-	if (password.empty())
-	{
-		std::cerr << "Password cannot be empty" << std::endl;
-    	return 1;
-	}
-	if (password.length() > 32)
-		std::cerr << "Password is longer than 32 characters." << std::endl;
-	for (size_t i = 0; i < password.length(); ++i)
-	{
-    	if (password[i] < 33 || password[i] > 126 || password[i] == 44 || password[i] == 58 || password[i] == 92 || password[i] == 39 || password[i] == 34)
-    	{
-        	std::cerr << "Password contains invalid character or is longer than 32 characters " << std::endl;
-        	return 1;
-    	}
-	}
-	std::cout << "calling server constructor" << std::endl;
 	try
 	{
+		if (argc < 3)
+			throw std::invalid_argument("Not enough parameters");
+		//haciendo isstringstream comprobamos automaticamente posibles errores de conversion y !portav >> port_num lo convierte a int
+		std::istringstream port_av(argv[1]);
+		if (!(port_av >> port_num) || port_num <= 0 || port_num > 65535)
+			throw std::invalid_argument("Invalid port number");
+		password = argv[2];
+		if (password.empty())
+			throw std::invalid_argument("Password cannot be empty");
+		if (password.length() > 32)
+			throw std::invalid_argument("Password is longer than 32 characters.");
+		for (size_t i = 0; i < password.length(); ++i)
+		{
+			if (password[i] < 33 || password[i] > 126 || password[i] == 44 || password[i] == 58 || password[i] == 92 || password[i] == 39 || password[i] == 34)
+				throw std::invalid_argument("Password contains invalid character");
+		}
+		std::cout << "calling server constructor" << std::endl;
 		Server	ircServer(port_num, password);
 		ircServer.run();
+	}
+	catch (std::invalid_argument &e)
+	{
+		std::cerr << "Problem with statrting parameters: " << e.what() << std::endl;
+		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+		exit (EXIT_FAILURE);
 	}
 	catch (std::runtime_error &e)
 	{
