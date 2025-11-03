2fdcfa1c6a206b0adc87aa12f2d397f8042050ca
Chris Haikney <chaikney@c4r6s1.42urduliz.com>
2025-11-03 17:12:00 +0100

    AWAY tidying and consolidation

--- source/Away.cpp
+++ source/Away.cpp
@@ -6,12 +6,6 @@
 
 # include <iostream>
 
-// AwaAwayite(void)
-// {
-// 	std::cerr << "Bare Away constructor should not be called" << std::endl;
-// }
-
-// FIXED? Away sends a NEEDMOREPARAMS error
 Away::Away(Server* srv, Message &seed) : ACommand(srv, seed, 0, 1)
 {
 	std::cerr << "Bare Away constructor called, hope that is not a problem..." << std::endl;
@@ -20,25 +14,24 @@ Away::Away(Server* srv, Message &seed) : ACommand(srv, seed, 0, 1)
 Away::~Away(void) {}
 
 // No, or empty parameter = NOT away
-// otherwise: going away, broadcast message
-// TODO Handle going-away message (e.g. broadcast to channel)
-// DONE? *Store* going away message for use in RPL_AWAY
+// otherwise: going away, set message and status
+// NOTE Not going to handle going-away message broadcast to channel
 void	Away::executeCmd(void)
 {
 	User*	usr = _msg.getOrigin();
-	if (_msg.getParams().empty() && (usr->isAway()))
+	if (usr)
 	{
-		usr->setAway(false);
-		this->_responses.push(Message::_reply(_msg, RPL_UNAWAY));
-	}
-	else if (!usr->isAway())
-	{
-		std::string	farewell_note = _msg.getParams().back();
-		usr->setAway(true);
-		usr->setAwayMsg(farewell_note);
-		this->_responses.push(Message::_reply(_msg, RPL_NOWAWAY));
-		// TODO We need to get *all* the User's channels
-		// TODO If the user is Invisible, do we say anything?
-//		this->_responses.push(Message::_channelMessage(*msg, chan));
+		if (_msg.getParams().empty() && (usr->isAway()))
+		{
+			usr->setAway(false);
+			this->_responses.push(Message::_reply(_msg, RPL_UNAWAY));
+		}
+		else if (!usr->isAway())
+		{
+			std::string	farewell_note = _msg.getParams().back();
+			usr->setAway(true);
+			usr->setAwayMsg(farewell_note);
+			this->_responses.push(Message::_reply(_msg, RPL_NOWAWAY));
+		}
 	}
 }
