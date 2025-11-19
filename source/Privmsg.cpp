d95299c155d00e1d58b73ce1541d9e632d0180df
Chris Haikney <chaikney@c4r6s1.42urduliz.com>
2025-11-19 14:13:50 +0100

    PRIVMSG: Add direct messge workaround for Hexchat
    It needs a space after the : in a direct message.
    Without that, the first word does not appear in the recipient's chat
    window.
    (Although it was received according to the Raw Log)

--- source/Privmsg.cpp
+++ source/Privmsg.cpp
@@ -1,15 +1,9 @@
 #include "ACommand.hpp"
 #include "Privmsg.hpp"
 
-# include <iostream>
 # include "Channel.hpp"
 # include "User.hpp"
 #include "ReplyEnums.hpp"
-
-// Privmsg::Privmsg(void)
-// {
-// 	std::cerr << "Bare Privmsg constructor should not be called" << std::endl;
-// }
 
 // Command: PRIVMSG
 // Parameters: <target>{,<target>} <text to be sent>
@@ -17,10 +11,9 @@
 // When the PRIVMSG message is sent from a server to a client and <target> starts with a dollar character ('$', 0x24), the message is a broadcast sent to all clients on one or multiple servers.
 // Optional: If <target> is a channel name, it may be prefixed with one or more channel membership prefix character (@, +, etc) and the message will be delivered only to the members of that channel with the given or higher status in the channel. Servers that support this feature will list the prefixes which this is supported for in the STATUSMSG RPL_ISUPPORT parameter, and this SHOULD NOT be attempted by clients unless the prefix has been advertised in this token.
 // Aquí va la lógica para enviar mensajes privados o a canales
-// TODO There are further checks needed on whether a message is allowed, see docs
+// IDEA Could be further checks on sendability if had Invisible etc
 // Sends a message to user(s) or channel(s)
 // https://modern.ircdocs.horse/#privmsg-message
-// FIXME Mesage formatting is wrong. sender? Message must be precede by :
 Privmsg::Privmsg(Server* srv, Message &seed) : ACommand(srv, seed, 2, 2)
 {
 	std::cerr << "Privmsg constructor called" << std::endl;
@@ -30,18 +23,17 @@ Privmsg::~Privmsg(void) {}
 
 // If sent directly to  a user that is Away, reply RPL_AWAY
 // If sent to a channel, check membership
-// TODO Split into Channel / User methods?
-// TODO Allow for multiple targets?
-// FIXME Set up of chat in clients not really working
-// FIXME To workaround Hexchat private conversation losing the first word, prefix with a space?
+// IDEA Split into Channel / User methods?
+// IDEA Allow for multiple targets?
+// FIXED Set up of chat in clients not really working
 // ...this is so dumb.
 void	Privmsg::executeCmd(void)
 {
-	// TODO Would this work?
 	User*	sender = this->_msg.getOrigin();
 	if (!sender)
 	{
-		// message is from server, treat as Broadcast
+		// this should not happen!
+		return;
 	}
 	std::list<std::string> params = _msg.getParams();
 	// NOTE if there are mulltiple targets, target will be comma-separated
@@ -51,17 +43,13 @@ void	Privmsg::executeCmd(void)
 	std::string msg_text = params.back();
 	if (target.empty())
 	{
-		// ERR_NORECIPIENT (411)
-		// 	DONE? Add text to message
-		// 	"<client> :No recipient given (<command>)"
 		this->_responses.push(Message::_reply(_msg, ERR_NORECIPIENT));
 		return;
 	}
 	if (target.find_first_of(",") != std::string::npos)
 	{
+		// NOTE Can't find a defintion of ERR_TOOMANYTARGETS, so choosing this instead
 		this->_responses.push(Message::_reply(_msg, ERR_NOSUCHCHANNEL));
-		// NOTE Can't find a defintion of ERR_TOOMANYTARGETS, so choosing the above instead
-		//this->_responses.push(Message::_reply(_msg, ERR_TOOMANYTARGETS));
 		return;
 	}
 	if (msg_text.empty())
@@ -79,20 +67,18 @@ void	Privmsg::executeCmd(void)
 			this->_responses.push(Message::_reply(_msg, ERR_NOSUCHCHANNEL));
 			return ;
 		}
-		// TODO Faster to find Channel membership directly with User? Or not?
+		// IDEA Faster to find Channel membership directly with User? Or not?
 		// Only allow if sender is member of the channel
 		if (!channel->isMember(sender))
 		{
-			// 404 ERR_CANNOTSENDTOCHAN
 			this->_responses.push(Message::_reply(_msg, ERR_CANNOTSENDTOCHAN));
 			return ;
 		}
 		else
-			_responses.push(Message::_channelMessage(_msg, channel));
+			this->_responses.push(Message::_channelMessage(_msg, channel));
 	}
 	else // Message to individual user
 	{
-		// TODO Need to change the text format in PRIVMSG e.g. source, or not?
 		User *to = this->_srv->findUserByNick(target);
 		if (to)
 		{
@@ -101,8 +87,10 @@ void	Privmsg::executeCmd(void)
 			else
 			{
 				Message*	dm = Message::_replyThirdParty(_msg, to);
-				dm->addParams(msg_text);
-				_responses.push(dm);
+				// NOTE To workaround Hexchat private conversation losing the first word,
+				// we prefix with a space.
+				dm->addParams(" " + msg_text);
+				this->_responses.push(dm);
 			}
 		}
 		else
